ac1dca41478e471444666aeef9e0eec703db0707
cyberpq <cyberpq@gmail.com>
2014-12-30 22:44:15 +0100

    Déplacement du code spécifique aux codeurs dans un fichier à part "encodeurs.cpp" Attention, malgrès l'extension, le code est en C et non en C++ pour le moment

---
+++ src_arduino/src/encodeurs.cpp
@@ -0,0 +1,72 @@
+#include "encodeurs.h"
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include "../lib/digitalWriteFast.h"
+
+//Pinout des codeurs en quadrature
+#define LEFT_A   2 // it externe IT_0
+#define LEFT_B   5
+#define RIGHT_A  3  // it externe
+#define RIGHT_B  4
+
+//variables
+static volatile int encoderLeftCount = 0;
+static volatile int encoderRightCount = 0;
+
+void encodeurs_reset()
+{
+  encoderLeftCount = 0;
+  encoderRightCount = 0;
+}
+
+void encoderInt_left() 
+{
+  if (digitalReadFast(LEFT_B) == HIGH)
+    encoderLeftCount++;
+  else
+    encoderLeftCount--;
+}
+
+void encoderInt_right() 
+{
+  if (digitalReadFast(RIGHT_B) == HIGH)
+    encoderRightCount++;
+  else
+    encoderRightCount--;
+}
+
+void encodeurs_setup()
+{
+      pinMode(LEFT_A,INPUT);
+      pinMode(LEFT_B,INPUT);
+      pinMode(RIGHT_A,INPUT);
+      pinMode(RIGHT_B,INPUT);
+      
+      attachInterrupt(0, encoderInt_left,  RISING);
+      attachInterrupt(1, encoderInt_right, RISING);
+}
+
+//-------------------------------------------------
+// Moyenne entre l'encodeur roite et gauche = 
+//   distance parcourue par le centre du robot
+int encodeurs_get_distance()
+{
+  return ( (encoderLeftCount+encoderRightCount) >> 1);
+}
+
+//-------------------------------------------------
+// Angle du robot (différence entre gauche et droite)
+// L'angle est positif en tournat vers la droite, comme
+// avec un compas géographique (=boussole)
+int encodeurs_get_angle()
+{
+  return (encoderLeftCount-encoderRightCount);
+}
+
+
+
---
+++ src_arduino/src/encodeurs.h
@@ -0,0 +1,5 @@
+void encodeurs_setup();
+int encodeurs_get_distance();
+int encodeurs_get_angle();
+void encodeurs_reset();
+
