70a880439c64088d7211ee0e9633b9ceab801a05
cyberpq <cyberpq@gmail.com>
2014-12-31 00:55:27 +0100

    Déplacement du code spécifique aux codeurs dans un fichier à part "encodeurs.cpp" Attention, malgrès l'extension, le code est en C et non en C++ pour le moment

--- src_arduino/src/essai_moteur.ino
+++ src_arduino/src/essai_moteur.ino
@@ -1,25 +1,23 @@
 #include "../lib/digitalWriteFast.h"
 #include "../lib/PID_v1.h"
 
+//#include <digitalWriteFast.h>
+//#include <PID_v1.h>
+
+#include "encodeurs.h"
+
 //Pinout des ponts en H de la carte
 #define MOTOR_LEFT_DIR  7
 #define MOTOR_LEFT_PWM  9
 #define MOTOR_RIGHT_DIR 8
 #define MOTOR_RIGHT_PWM 10
 
-//Pinout des codeurs en quadrature
-#define LEFT_A   2 // it externe IT_0
-#define LEFT_B   5
-#define RIGHT_A  3  // it externe
-#define RIGHT_B  4
-
 //Autres pins
 #define LED_BLEUE 13 // LED connected to digital pin 13
 
 //variables
 int val = 0;     // variable to store the read value
-volatile int encoderLeftCount = 0;
-volatile int encoderRightCount = 0;
+
 volatile boolean changeFlag = false;
 volatile int vitesse = 255;
 
@@ -31,6 +29,9 @@ int consigne_v_d = 0;
 
 void setup(){
   Serial.begin(115200);
+  
+  encodeurs_setup();
+  
   pinMode(MOTOR_LEFT_DIR,OUTPUT);
   pinMode(MOTOR_LEFT_PWM,OUTPUT);
   pinMode(MOTOR_RIGHT_DIR,OUTPUT);
@@ -38,16 +39,8 @@ void setup(){
 
   digitalWrite(MOTOR_LEFT_DIR , 1); 
   digitalWrite(MOTOR_RIGHT_DIR, 1);
-  
-  pinMode(LEFT_A,INPUT);
-  pinMode(LEFT_B,INPUT);
-  pinMode(RIGHT_A,INPUT);
-  pinMode(RIGHT_B,INPUT);
-  
+ 
   pinMode(LED_BLEUE, OUTPUT);      // sets the digital pin 13 as output
-  
-   attachInterrupt(0, encoderInt_left,  RISING);
-   attachInterrupt(1, encoderInt_right, RISING);
    
    Serial.print("Batterie : ");
    // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
@@ -80,7 +73,7 @@ void GestionRxOrdre()
     
     switch(incomingByte)
     {
-      case '0' : setMotor(0      , 0      ); encoderLeftCount=0; encoderRightCount=0;break;
+      case '0' : setMotor(0      , 0      ); encodeurs_reset(); break;
       case '1' : setMotor(0      , 0      ); break;
       case '2' : setMotor(-vitesse, -vitesse); break;
       case '3' : setMotor(0      , 0      ); break;
@@ -92,6 +85,8 @@ void GestionRxOrdre()
       case '9' : setMotor(+vitesse, vitesse>1); break;
       case '+' : vitesse=constrain(vitesse+10, 0, 255); break;
       case '-' : vitesse=constrain(vitesse-10, 0, 255); break;
+	case 'p':
+	case 'P': changeFlag = true; break;
       default  : Serial.println("commande inconnue !");
     }
   }
@@ -130,10 +125,10 @@ void GestionTxStatus()
   {
     digitalWrite(LED_BLEUE, 1);
     changeFlag = false;
-    Serial.print("L:");
-    Serial.print(encoderLeftCount);
-    Serial.print("  R:");
-    Serial.print(encoderRightCount);
+    Serial.print("D:");
+    Serial.print(encodeurs_get_distance());
+    Serial.print("  a:");
+    Serial.print(encodeurs_get_angle());
     Serial.print("  vg:");
     Serial.print(cmd_v_g);
     Serial.print("  vd:");
@@ -142,20 +137,4 @@ void GestionTxStatus()
   }
 }
 
-void encoderInt_left() 
-{
-  if (digitalReadFast(LEFT_B) == HIGH)
-    encoderLeftCount++;
-  else
-    encoderLeftCount--;
-  changeFlag = true;
-}
 
-void encoderInt_right() 
-{
-  if (digitalReadFast(RIGHT_B) == HIGH)
-    encoderRightCount++;
-  else
-    encoderRightCount--;
-  changeFlag = true;
-}
