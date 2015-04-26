0e3174bb8f51db2c4b8243c95d98401a1cd4e921
CyberPQ <CyberPQ@gmail.com>
2015-04-26 10:16:07 +0000

    Ajout d'un script python affichant le status du roa l'ecran

---
+++ src_raspberry/lcd_status.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+import Image
+import ImageDraw
+import ImageFont
+
+import Adafruit_ILI9341 as TFT
+import Adafruit_GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
+
+import socket
+
+# Raspberry Pi configuration.
+DC = 18
+RST = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# BeagleBone Black configuration.
+# DC = 'P9_15'
+# RST = 'P9_12'
+# SPI_PORT = 1
+# SPI_DEVICE = 0
+
+# Create TFT LCD display class.
+disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
+
+# Initialize display.
+disp.begin()
+
+# Clear the display
+# Can pass any tuple of red, green, blue values (from 0 to 255 each).
+disp.clear((0, 0, 50))
+
+# Get a PIL Draw object to start drawing on the display buffer.
+draw = disp.draw()
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Define a function to create rotated text.  Unfortunately PIL doesn't have good
+# native support for rotated fonts, but this function can be used to make a 
+# text image and rotate it so it's easy to paste in the buffer.
+def draw_rotated_text(image, text, position, angle, font, fill=(255,255,255)):
+	# Get rendered font width and height.
+	draw = ImageDraw.Draw(image)
+	width, height = draw.textsize(text, font=font)
+	# Create a new image with transparent background to store the text.
+	textimage = Image.new('RGBA', (width, height), (0,0,0,0))
+	# Render the text.
+	textdraw = ImageDraw.Draw(textimage)
+	textdraw.text((0,0), text, font=font, fill=fill)
+	# Rotate the text image.
+	rotated = textimage.rotate(angle, expand=1)
+	# Paste the text into the image, using it as a mask for transparency.
+	image.paste(rotated, position, rotated)
+
+
+def get_lan_ip():
+    ip = ""
+    print([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][0])
+    return ip
+
+# Write two lines of white text on the buffer, rotated 90 degrees counter clockwise.
+draw_rotated_text(disp.buffer, socket.gethostname(), 	(30, 20), 90, font, fill=(255,255,255))
+draw_rotated_text(disp.buffer, get_lan_ip(), 		(50, 20), 90, font, fill=(255,255,255))
+
+
+# Write buffer to display hardware, must be called to make things visible on the
+# display!
+disp.display()
